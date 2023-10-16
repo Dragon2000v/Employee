6b171de57d4450bcb5bcfce51ca921c27dadfa70
Shmatko Vitaly <58611837+Dragon2000v@users.noreply.github.com>
2023-10-16 17:01:54 +0300

    Delete React_NET_API/JwtWebApiDotNet7 directory
    f

--- React_NET_API/JwtWebApiDotNet7/JwtWebApiDotNet7/Models/UserDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace JwtWebApiDotNet7.Models
-{
-    public class UserDto
-    {
-        public required string Username { get; set; }
-        public required string Password { get; set; }
-    }
-}
